4cbdbdab606d5f7ed1ca9907ef5125564798af6f
GabDonzelli <goliveirad@sara-e37-03>
2026-07-24 14:17:43 +0200

    Selected Mode

--- pytorchexample/server_app.py
+++ pytorchexample/server_app.py
@@ -5,6 +5,8 @@ from flwr.app import ArrayRecord, ConfigRecord, Context, MetricRecord, MessageTy
 from flwr.serverapp import Grid, ServerApp
 from flwr.serverapp.strategy import FedAvg
 
+import random
+
 from pytorchexample.task import Net, load_centralized_dataset, test
 
 # Create ServerApp
@@ -15,84 +17,63 @@ app = ServerApp()
 
 #create a customized strategy to be able to get the node and partition relation
 class CustomFedAvg(FedAvg):
-    def __init__(self, client_to_node, **kwargs):
+
+    def __init__(self, selection_mode, clients_per_round, random_seed, client_to_node, **kwargs):
         super().__init__(**kwargs)
         # Dictionary to store partition-to-node mapping
         self.client_to_node = client_to_node  
-        # Dictionary to store client names
-        #self.client_names = {}  
+        self.selection_mode = selection_mode
+        self.clients_per_round = clients_per_round
+        self.random_seed = random_seed
+        self.rng = random.Random()
 
     def configure_train(self, server_round, arrays, config, grid):
         print(f"Configuring training for server round {server_round}...")
-
-        # first round == mapping
-        if server_round == 1:
-
-            #to name clients 
-            #for client_number, (client_id, node_id) in enumerate ( 
-                #sorted(self.client_to_node.items()), start = 1): 
-
-                #client_name = f"Client {client_number}" 
-                #self.client_names[node_id] = client_name
-
-                #print(f"{client_name} is on Node {node_id} with Partition {client_id}")
-
-
-            return super().configure_train(server_round, arrays, config, grid)
-        
-        if server_round == 2:
-            selected_clients = [1,2,3,4,5]
-            for i in range(len(selected_clients)):
-                client_id = selected_clients[i]
-                node_id = self.client_to_node[client_id]
-                print(f"Client selected: {selected_clients[i]} and is on Node {node_id} ")
-                                   
-        if server_round == 3:
-            selected_clients = [6,7,8,9,10]
-            #pode vir a virar uma funcao print###################################
-            for i in range(len(selected_clients)):
-                client_id = selected_clients[i]
-                node_id = self.client_to_node[client_id]
-                print(f"Client selected: {selected_clients[i]} and is on Node {node_id} ")
-
-        # get the node_id of the selected partitions and print them
+        # Get the list of available client 
+        available_client_ids = list(self.client_to_node.keys())
+        # Select clients based on the selection mode
+        selected_clients = self.select_client_ids(server_round, available_client_ids)
+        # Get the corresponding node IDs for the selected clients
         selected_nodes = [self.client_to_node[client_id] for client_id in selected_clients]
-
+        # Print the selected clients for this round
+        selected_names = [f"Client {client_id}" for client_id in selected_clients]
+        print(f"Round {server_round}: {', '.join(selected_names)}")
+              
         # update the config with the current server_round   
         config["server_round"] =  server_round
-
         #sending the global model to the selected nodes
         record = RecordDict({self.arrayrecord_key: arrays, self.configrecord_key: config, })
-
         #record = what is sent to the selected nodes, selected_nodes = the nodes that will receive the record
         return self._construct_messages(record, selected_nodes,  MessageType.TRAIN)
-        
 
     def aggregate_train(self, server_round, replies):
         replies = list(replies)
-
         for reply in replies: 
             if reply.has_content(): 
                 client_id = int (reply.content["metrics"]["partition-id"])
-                
                 client_id = client_id + 1
                 node_id = reply.metadata.src_node_id
-                
                 # Store the mapping
                 # only save the mapping once per client
                 if client_id not in self.client_to_node:
                     self.client_to_node[client_id] = node_id 
-
                 # Remove partition-id from metrics
                 reply.content["metrics"].pop("partition-id", None)  
-        
-        
-        if server_round == 1:
-            print(f"\nMapping round {server_round}:")
-            for client_id, node_id in sorted(self.client_to_node.items()):
-                print(f"Client {client_id}: Node {node_id}")
-
         return super().aggregate_train(server_round, replies)
+
+    #create a function to select the clients based on the selection mode
+    def select_client_ids (self, server_round, available_clients_ids):
+        client_ids = sorted (available_clients_ids)
+        if self.selection_mode == "random":
+            number_elected = min(self.clients_per_round, len(client_ids))
+            rng = random.Random(self.random_seed + server_round)
+            return self.rng.sample(client_ids, number_elected)
+        if self.selection_mode == "odd-even":
+            if server_round % 2 == 1:
+                return [client_id for client_id in client_ids if (client_id + 1) % 2 == 1]
+            else:
+                return [client_id for client_id in client_ids if (client_id + 1) % 2 == 0]
+        raise ValueError(f"Unknown selection mode: {self.selection_mode}")
 
 
 @app.main()
@@ -106,6 +87,9 @@ def main(grid: Grid, context: Context) -> None:
     fraction_evaluate: float = context.run_config["fraction-evaluate"]
     num_rounds: int = context.run_config["num-server-rounds"]
     lr: float = context.run_config["learning-rate"]
+    selection_mode =str(context.run_config["selection-mode"])
+    clients_per_round = int(context.run_config["clients-per-round"])
+    random_seed = int(context.run_config["random-seed"])
 
     # Show mapping of clients and nodes
     node_ids = list(grid.get_node_ids())
@@ -134,6 +118,9 @@ def main(grid: Grid, context: Context) -> None:
         min_train_nodes = 2,
         min_evaluate_nodes = 2,
         min_available_nodes = 2,
+        selection_mode=selection_mode,
+        clients_per_round=clients_per_round,
+        random_seed =random_seed,
     )
 
     # Start strategy, run FedAvg for `num_rounds`
